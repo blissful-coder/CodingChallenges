a8ab77355cfe47117de099f24ca37567888eae34
blissful_coder <anurag.negi1729@gmail.com>
2026-01-02 12:45:34 +0530

    code(init): initial code a spliting the line for tabs

---
+++ challenge-cut/cut.cpp
@@ -0,0 +1,57 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+
+std::vector<std::string> divideInParts(const std::string& line);
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " -f<list> <filename>\n";
+        return EXIT_FAILURE;
+    }
+
+    std::ifstream ifs(argv[2]);
+    if (!ifs)
+    {
+        std::cerr << "Error: couldn't open file for reading\n";
+        return EXIT_FAILURE;
+    }
+
+    int line_no=0;
+    for(std::string line; getline(ifs,line); line_no++){
+        // std::cout<<"\n"<<line;
+        std::vector<std::string> lineparts = divideInParts(line);
+        
+        std::cout<<"\n Line:"<<line_no<<" ";
+        for (size_t i =0; i < lineparts.size(); i++){
+            std::cout<<"["<<lineparts[i]<<"]";
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
+
+std::vector<std::string> divideInParts(const std::string& line){
+    std::vector<std::string> parts;
+    size_t start = 0;
+    
+    for(size_t i = 0; i < line.size(); i++){
+        if(line[i] == '\t'){
+            // Extract substring from start to current position (length = i - start)
+            std::string temp = line.substr(start, i - start);
+            parts.push_back(temp);
+            start = i + 1;
+        }
+    }
+    
+    // Don't forget the last part (after the final tab or the entire string if no tabs)
+    if(start <= line.size()){
+        std::string temp = line.substr(start);
+        parts.push_back(temp);
+    }
+    
+    return parts;
+}
